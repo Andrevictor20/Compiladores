767e59a81fd85fad177a4b78f8fea45bdb0bd7ce
André Victor Macedo Pereira <victorandre280@gmail.com>
2025-09-29 23:45:54 +0000

    Criação do pacote e classe parser

--- tradutor/src/main/java/com/compiladores/main/Main.java
+++ tradutor/src/main/java/com/compiladores/main/Main.java
@@ -1,9 +1,10 @@
-package com.compiladores;
+package com.compiladores.main;
 
+import com.compiladores.parser.Parser;
 public class Main {
     public static void main(String[] args) throws Exception {
         String input = "8+5-7+9";
-        Parser p = new Parser (input.getBytes());
+        Parser p = new Parser(input.getBytes());
         p.parse();
 
     }
---
+++ tradutor/src/main/java/com/compiladores/parser/Parser.java
@@ -0,0 +1,16 @@
+package com.compiladores.parser;
+
+
+public class Parser {
+    private byte[] input;
+    private int current; 
+
+	public Parser (byte[] input) {
+        this.input = input;
+    }
+
+    public void parse () {
+        // aqui ainda ira o código
+    }
+
+}
