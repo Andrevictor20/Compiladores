385d305a382818fee15eed06f1be4508da4c7701
André Victor Macedo Pereira <victorandre280@gmail.com>
2025-09-30 00:06:03 +0000

    Criação do pacote/classe scanner (analisador lexico)

---
+++ tradutor/src/main/java/com/compiladores/scanner/Scanner.java
@@ -0,0 +1,12 @@
+package com.compiladores.scanner;
+
+public class Scanner {
+
+    private byte[] input;
+    private int current; 
+
+	public Scanner (byte[] input) {
+        this.input = input;
+    }
+
+}
