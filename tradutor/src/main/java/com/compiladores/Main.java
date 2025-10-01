1465745d50d79658c233cc989480cbb535524fb8
André Victor Macedo Pereira <victorandre280@gmail.com>
2025-10-01 23:49:11 +0000

    Teste de string com espacos na main

--- tradutor/src/main/java/com/compiladores/Main.java
+++ tradutor/src/main/java/com/compiladores/Main.java
@@ -1,14 +1,12 @@
 //"https://profsergiocosta.notion.site/Tradu-o-dirigida-por-sintaxe-bc590c67d8234f81bee5cfdb505f2dd1"
 package com.compiladores;
+
 import com.compiladores.parser.Parser;
 
 public class Main {
-   public static void main(String[] args) {
-        String input = "89+508-7+99";
+    public static void main(String[] args) {
+        String input = "45  + 89   -       876";
         Parser p = new Parser(input.getBytes());
         p.parse();
     }
 }
-
-
-
