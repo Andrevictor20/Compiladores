98fb38f7b0f6ef8ca89d75c12f9c94e3c4b7dc47
André Victor Macedo Pereira <victorandre280@gmail.com>
2025-10-02 00:00:09 +0000

    Teste apos alterações do scanner na main

--- tradutor/src/main/java/com/compiladores/Main.java
+++ tradutor/src/main/java/com/compiladores/Main.java
@@ -1,12 +1,16 @@
 //"https://profsergiocosta.notion.site/Tradu-o-dirigida-por-sintaxe-bc590c67d8234f81bee5cfdb505f2dd1"
 package com.compiladores;
 
-import com.compiladores.parser.Parser;
+import com.compiladores.scanner.Scanner;
+import com.compiladores.scanner.Token;
+import com.compiladores.scanner.TokenType;
 
 public class Main {
     public static void main(String[] args) {
         String input = "45  + preco - 876";
-        Parser p = new Parser(input.getBytes());
-        p.parse();
+        Scanner scan = new Scanner(input.getBytes());
+        for (Token tk = scan.nextToken(); tk.type != TokenType.EOF; tk = scan.nextToken()) {
+            System.out.println(tk);
+        }
     }
 }
